a64a9ec3eaca6ec6a88314304d1d19e3bad85505
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-08-27 11:38:50 +0000

    Test XMLRearder 추가

--- sbgProject/Assets/Script/Item/XMLReader.cs
+++ sbgProject/Assets/Script/Item/XMLReader.cs
@@ -1,15 +1,29 @@
 using UnityEngine;
 using System.Collections;
 
-public class XMLReader : MonoBehaviour {
+interface IXMLReader
+{
+    void Start();
+    void Update();
+}
+
+public class XMLReader : MonoBehaviour, IXMLReader
+{
 
 	// Use this for initialization
-	void Start () {
+	public void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    public void Update()
+    {
 	
 	}
+}
+
+public class TestXMLReader : IXMLReader
+{
+    public void Start() { }
+    public void Update() { }
 }
