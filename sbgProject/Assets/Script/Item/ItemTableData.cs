ffc302f44b1990a081a960532b6ad56f15cdc1eb
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-09-04 12:40:27 +0000

    임시로 올림

---
+++ sbgProject/Assets/Script/Item/ItemTableData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+using System.Xml;
+using System.IO;
+using System.Text;
+
+public class ItemTableData
+{
+    private XmlNode m_XmlNode;
+    private int m_Index;
+
+    public XmlNode XMLNode
+    {
+        get
+        {
+            return m_XmlNode;
+        }
+    }
+
+    public int Index
+    {
+        get
+        {
+            return m_Index;
+        }
+    }
+
+    public ItemTableData(XmlElement xmlElement)
+    {
+        m_XmlNode = xmlElement;
+        SetValue(ref m_Index, m_XmlNode, "Index", 0);
+    }
+
+    public void SetValue(ref int _target, XmlNode _node, string _column, int _def)
+    {
+        _target = int.Parse(_node[_column].InnerText);
+    }
+
+}
+
--- sbgProject/Assets/Test/Test_ItemTableData.cs
+++ sbgProject/Assets/Test/Test_ItemTableData.cs
@@ -1,15 +1,32 @@
 using UnityEngine;
 using System.Collections;
+using NUnit.Framework;
+using System.Xml;
 
-public class Test_ItemTableData : MonoBehaviour {
+namespace Test
+{
+    [TestFixture]
+    public class Test_ItemTableData
+    {
+        [Test]
+        public void Initialize()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            Assert.NotNull(xmlDoc);
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+            string xmlFile =
+            (
+                "<Root><name>wrench</name></Root>"
+            );
+
+            XmlElement newElement = xmlDoc.CreateElement("price");
+            Assert.NotNull(newElement);
+            newElement.InnerText = "10.95";
+            xmlDoc.DocumentElement.AppendChild(newElement);
+
+            xmlDoc.LoadXml(xmlFile);
+
+        }
+    }
+
 }
