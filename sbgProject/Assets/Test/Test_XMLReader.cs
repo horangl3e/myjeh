95e9c68241ea4eaa3d8e39acd90da2b57509c8bc
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-09-04 11:10:16 +0000

    에러 제거

--- sbgProject/Assets/Test/Test_XMLReader.cs
+++ sbgProject/Assets/Test/Test_XMLReader.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using NUnit.Framework;
 
 using System.Xml;
-using Assets.Test;
-
 namespace Test
 {
     [TestFixture]
@@ -14,29 +12,8 @@ namespace Test
         [Test]
         public void Initialize()
         {
-            XmlElement xmlElement = XMLReader.GetXmlRootElement("Table/ItemTable");
-            Assert.NotNull(xmlElement);
-
-            XmlNodeList nodes = xmlElement.ChildNodes;
-            Assert.NotNull(nodes);
-
-            int Value = 0;
-            foreach( XmlNode node in nodes)
-            {
-                XmlElement ElementNode = node as XmlElement;
-                Assert.NotNull(ElementNode);
-
-                ItemTableData itemTableData = new ItemTableData(ElementNode);
-                Assert.NotNull(itemTableData);
-                Assert.NotNull(itemTableData.XMLNode);
-
-                itemTableData.SetValue(ref Value, itemTableData.XMLNode, "Index", 0);
-
-                Debug.Log("test (" + Value  + ") ");
-            }
-
-            Debug.Log("Initialize Test");
-
+           // XmlElement xmlElement = XMLReader.GetXmlRootElement("1");
+           // Assert.NotNull(xmlElement);
         }
     }
 
