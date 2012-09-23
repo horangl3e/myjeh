2b75925ce325610dae9a424874680154d42cbed5
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-09-23 12:51:36 +0000

    Add test code

--- sbgProject/Assets/Test/Test_XMLReader.cs
+++ sbgProject/Assets/Test/Test_XMLReader.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 using System.Collections;
 using NUnit.Framework;
 using System.Collections.Generic;
-
 using System.Xml;
+
 namespace Test
 {
     [TestFixture]
-    public class Test_XMLReader
+    public class Test_XMLReader : XMLReader
     {
-
         [Test]
         public void Initialize()
         {
@@ -17,12 +16,36 @@ namespace Test
             Assert.NotNull(xmlElement);
 
             XmlNodeList nodes = xmlElement.ChildNodes;
+            int count = 0;
+            int index = 0;
+            string strName = "";
+            string type = "";
+
             foreach (XmlNode node in nodes)
             {
                 XmlElement xmlelement = (node as XmlElement);
                 Assert.NotNull(xmlelement);
-            }
 
+                SetValue(ref index, node, "Index");
+                SetValue(ref strName, node, "Name");
+                SetValue(ref type, node, "Type");
+
+                if (count == 0)
+                {
+                    Assert.That(index, Is.EqualTo(1));
+                    Assert.That(strName, Is.EqualTo("MpUp"));
+                    Assert.That(type, Is.EqualTo("HP_AMOUNT"));
+                }
+
+                if (count == 1)
+                {
+                    Assert.That(index, Is.EqualTo(2));
+                    Assert.That(strName, Is.EqualTo("Mpdown"));
+                    Assert.That(type, Is.EqualTo("HP_AMOUNT"));
+                }  
+                 
+                count++;
+            }
         }
     }
 
