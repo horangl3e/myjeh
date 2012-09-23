0cefd639b384698d9487a817dd4a93d330fe5877
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-09-23 12:54:30 +0000

    Add GetFunction

--- sbgProject/Assets/Script/Item/Item.cs
+++ sbgProject/Assets/Script/Item/Item.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class Item
 {
     private ItemData m_ItemData;
+
+    public ItemData Data
+    {
+        get
+        {
+            return m_ItemData;
+        }
+    }
     public Item( ItemData itemData )
     {
         m_ItemData = itemData;
