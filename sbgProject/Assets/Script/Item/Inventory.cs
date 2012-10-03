5a60c9447b471f4b990294900df3efbfd63d5baa
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-10-03 14:38:40 +0000

    Test Add_Item

--- sbgProject/Assets/Script/Item/Inventory.cs
+++ sbgProject/Assets/Script/Item/Inventory.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Inventory {
+
+    private List<Item> ItemList = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        ItemList.Add(item);
+    }
 }
--- sbgProject/Assets/Test/Test_Inventory.cs
+++ sbgProject/Assets/Test/Test_Inventory.cs
@@ -1,16 +1,39 @@
 using UnityEngine;
 using System.Collections;
 using NUnit.Framework;
+using System.Xml;
 
 namespace Test
 {
     [TestFixture]
     public class Test_Inventory
     {
-        [Test]
-        public void Initialize()
-        {
+        private XmlNodeList xmlNodelist;
+        private Inventory inventory;
 
+        [SetUp]
+        public void SetUp()
+        {
+            inventory = new Inventory();
+            Assert.NotNull(inventory);
+
+            XmlElement xmlElement = XMLReader.GetXmlRootElement("Table/ItemTable");
+            Assert.NotNull(xmlElement);
+
+            xmlNodelist = xmlElement.ChildNodes;
+            Assert.NotNull(xmlNodelist);
+        }
+
+        [Test]
+        public void Test_AddItem()
+        {
+            ItemData itemData = new ItemData(xmlNodelist[0] as XmlElement);
+            Assert.NotNull(itemData);
+
+            Item item = new Item(itemData);
+            Assert.NotNull(item);
+
+            inventory.AddItem(item);
         }
 
     }
