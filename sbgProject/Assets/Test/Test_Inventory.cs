300bc2dc741ee4a07b8de1f7918446be219aa548
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-10-03 11:55:42 +0000

    파일 추가

---
+++ sbgProject/Assets/Test/Test_Inventory.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test_Inventory
+    {
+        [Test]
+        public void Initialize()
+        {
+
+        }
+
+    }
+}
+
