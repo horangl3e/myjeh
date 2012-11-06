cfa343df211140a43706296598a4515bae42a610
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-11-06 08:52:19 +0000

    dd

--- TestProject/Assets/Scene/JumpTest/FSMState.cs
+++ TestProject/Assets/Scene/JumpTest/FSMState.cs
@@ -5,6 +5,8 @@ public class FSMState : MonoBehaviour {
 
     private int m_iStateID;
     private int m_usNumberOfTransistions;
+    private ArrayList m_InputList = new ArrayList();
+    private ArrayList m_OutPutState = new ArrayList();
 
     public void Initialize( int iStateID, int Transitions )
     {
@@ -14,9 +16,15 @@ public class FSMState : MonoBehaviour {
             m_usNumberOfTransistions = Transitions;
 
         m_iStateID = iStateID;
+
+        for (int i = 0; i < m_usNumberOfTransistions; i++)
+        {
+            m_InputList.Add(0);
+            m_OutPutState.Add(0);
+        }
     }
 
-	// Use this for initialization
+	// Use this for initializationㅑ
 	void Start () {
 	
 	}
@@ -33,6 +41,47 @@ public class FSMState : MonoBehaviour {
 
     public int GetOutPut( int iInput )
     {
-        return m_iStateID;
+        //내 스테이트를 일단 지정해놓는다.
+        int iOutPutID = m_iStateID;
+        for (int i = 0; i < m_usNumberOfTransistions; ++i )
+        {
+            if ((int)m_OutPutState[i] == 0)
+                break;
+
+            //특정 트랜지션이 어느 스테이트로 가는지에 대한 ID를 얻는다
+            if (iInput == (int)m_InputList[i])
+            {
+                iOutPutID = (int)m_OutPutState[i];
+                break;
+            }
+        }
+        return iOutPutID;
+    }
+
+    public void AddTransition(int iInput, int iOutputID)
+    {
+        //값이 비어 있는게 있다면 비어 있는 곳에 세팅 한다.
+        //값이 다 차있으면 더이상 추가를 하지 않는다.
+        int i = 0;
+        for (; i < m_usNumberOfTransistions; ++i)
+        {
+            if ((int)m_OutPutState[i] == 0)
+                break;
+        }
+
+        if (i < m_usNumberOfTransistions)
+        {
+            m_OutPutState[i] = iOutputID;
+            m_InputList[i] = iInput;
+        }
+    }
+
+    public void DeleteTransition(int iOutputID)
+    {
+        for (int i = 0; i < m_usNumberOfTransistions; ++i )
+        {
+           // if( m_OutPutState[i] == iOutputID )
+
+        }
     }
 }
--- TestProject/Assets/Test/Test_FSMState.cs
+++ TestProject/Assets/Test/Test_FSMState.cs
@@ -10,20 +10,26 @@ namespace Test
         private GameObject gameObject;
 
         [SetUp]
-        void SetUp()
+        public void SetUp()
         {
             gameObject = new GameObject();
             Assert.NotNull(gameObject);
         }
         
         [Test]
-        void Test_Initialize()
+        public void Test_Initialize()
         {
             FSMState FsmState = gameObject.AddComponent<FSMState>();
             Assert.NotNull(FsmState);
 
             FsmState.Initialize(100, 8);
             Assert.That( 100, Is.EqualTo( FsmState.GetID() ) );
+
+            object[] testArrayList = new object[] {1091,123,123};
+            Assert.That(1091, Is.EqualTo(testArrayList[0]));
+
+            ArrayList array = new ArrayList { 1, 2, 3, 4, 6, "asd", "asdasd"};
+            Assert.That(3, Is.EqualTo(array[2]));
         }
     }
 }
