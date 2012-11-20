12a8a081b65ce29730cfc84536d07c3927ae1c16
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-11-20 23:13:15 +0000

    hh

--- TestProject/Assets/Script/UI/ImageFont.cs
+++ TestProject/Assets/Script/UI/ImageFont.cs
@@ -4,32 +4,11 @@ using System.Collections.Generic;
 
 public class ImageFont : MonoBehaviour {
 
-    public Dictionary<string, UIAtlas.Sprite> DictionarySprite;
+    public Dictionary<string, UIAtlas.Sprite> DictionarySprite = new Dictionary<string, UIAtlas.Sprite>();
 
 	// Use this for initialization
 	void Start () {
-        //0부터 이제 숫자 9까지 스프라이트를 insert 했다.
-        UISprite uiSprite = gameObject.GetComponent<UISprite>();
-        if (uiSprite)
-        {
-            UIAtlas uiAtlas = uiSprite.atlas;
-            if (uiAtlas)
-            {
-                List<UIAtlas.Sprite> sprite = uiAtlas.spriteList;
-                if (sprite.Count != 0)
-                {
-                    int i = 0;
-                    foreach (UIAtlas.Sprite ObjectSprite in sprite)
-                    {
-                        DictionarySprite.Add(i.ToString(), ObjectSprite);
-                        i++;
-                    }
-                }
-            }
 
-        }
-
-        SetTest();
 	}
 	
 	// Update is called once per frame
@@ -37,17 +16,39 @@ public class ImageFont : MonoBehaviour {
 	
 	}
 
-    void SetTest()
+    public void SetTest()
     {
-       int i = 9;
+       //0부터 이제 숫자 9까지 스프라이트를 insert 했다.
+       UISprite uiSprite = gameObject.GetComponent<UISprite>();
 
-       if (DictionarySprite.ContainsKey(i.ToString()))
+       if (uiSprite)
        {
-          UIAtlas.Sprite sprite = DictionarySprite[i.ToString()];
-          gameObject.GetComponent<UISprite>().spriteName = sprite.name;
+           UIAtlas uiAtlas = uiSprite.atlas;
+           if (uiAtlas)
+           {
+               List<UIAtlas.Sprite> sprite = uiAtlas.spriteList;
+               if (sprite.Count != 0)
+               {
+                   int i = 0;
+                   foreach (UIAtlas.Sprite ObjectSprite in sprite)
+                   {
+                       DictionarySprite.Add(i.ToString(), ObjectSprite);
+                       i++;
+                   }
+               }
+           }
+
        }
 
-
+       int j = 9;
+       //이제 각 스프라이트 마다 숫자를 전달 해줘야 한다..
+       //11123412 이라면 맨 끝에 부터 토근 별로 얻어 온다음에 
+       Debug.Log(" DictionarySprite.Count = " + DictionarySprite.Count);
+       if (DictionarySprite.ContainsKey(j.ToString()))
+       {
+           UIAtlas.Sprite sprite = DictionarySprite[j.ToString()];
+          gameObject.GetComponent<UISprite>().spriteName = sprite.name;
+       }
 
     }
 }
---
+++ TestProject/Assets/Script/UI/TestCode/Test_ImageFont.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Test
+{
+    [TestFixture]
+    public class Test_ImageFont
+    {
+        public GameObject gameObject;
+
+        [Test]
+        public void Initialize()
+        {
+            GameObject objectBtn = GameObject.Find("Sprite (+BTN_00)");
+            Assert.NotNull(objectBtn);
+
+            objectBtn.AddComponent<ImageFont>();
+
+            ImageFont imageFont = objectBtn.GetComponent<ImageFont>();
+
+            Assert.NotNull(imageFont);
+
+            UISprite uiSprite = objectBtn.GetComponent<UISprite>();
+            Assert.NotNull(uiSprite);
+
+            imageFont.SetTest();
+
+        }
+
+    }
+}
+
+
