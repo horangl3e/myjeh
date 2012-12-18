de781d95479ea88e4e10486e30d9f14795a64c9c
yjlmm0505@gmail.com <yjlmm0505@gmail.com@18a227d9-0384-49cd-9144-1d7f36d20937>
2012-12-18 01:50:36 +0000

    크기에따라 리스트 사이즈 조정

---
+++ TestProject/Assets/Script/UI/CarSelectScene/ItemScale.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class ItemScale : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("GameObject LocalScale.x " + gameObject.transform.position.x);
+
+        gameObject.transform.localScale = new Vector3(gameObject.transform.position.x, gameObject.transform.position.x, gameObject.transform.position.x);
+	}
+}
